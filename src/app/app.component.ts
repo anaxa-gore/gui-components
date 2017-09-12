28bb75862b368327faa80d273e0b311e1b7b3038
A4811584 <thomas.bonavia@apave.com>
2017-09-12 21:14:45 +0200

    Exemple d'utilisation du premier composant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'gui-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gui works!';
+  toto = 'my dude';
+
+  // listener: Observable<string>;
+
+  dataChange(val: string) {
+    console.log('valeur en top = ', val);
+  }
+
+  ngOnInit(): void {
+    // this.listener.subscribe(val => this.title = val);
+  }
 }
