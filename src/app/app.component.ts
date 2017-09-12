30d8bd46bc2d9cc124f13361dfca9b017db01c8a
A4811584 <thomas.bonavia@apave.com>
2017-09-12 21:23:28 +0200

    Nettoyage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,10 @@ export class AppComponent implements OnInit {
   title = 'gui works!';
   toto = 'my dude';
 
-  // listener: Observable<string>;
-
   dataChange(val: string) {
     console.log('valeur en top = ', val);
   }
 
   ngOnInit(): void {
-    // this.listener.subscribe(val => this.title = val);
   }
 }
