ecd634a2b6a5b13e12a1f6592127acf802026373
A4811584 <thomas.bonavia@apave.com>
2017-09-25 11:40:07 +0200

    Ajout du fonctionnel sur la liste

--- src/app/simple-list-input/simple-list-input.component.ts
+++ src/app/simple-list-input/simple-list-input.component.ts
@@ -1,23 +1,27 @@
 import {Component, HostListener, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {ValidableInputComponent} from '../validable-input/validable-input.component';
 
 @Component({
   selector: 'gui-simple-list-input',
   templateUrl: './simple-list-input.component.html',
   styleUrls: ['./simple-list-input.component.css']
 })
-export class SimpleListInputComponent implements OnInit {
+export class SimpleListInputComponent extends ValidableInputComponent implements OnInit {
   @Input() label: string;
 
   group: FormGroup;
   items: Array<string> = ['Bonjour', 'Ca va', 'Pas de problème'];
+  selectedValue: string;
+
+
   // @HostListener('document:click') mouseClicked(evt) {
   //   console.log('hello');
   // }
 
 
   constructor(private fb: FormBuilder) {
-
+    super();
   }
 
   ngOnInit() {
@@ -26,4 +30,7 @@ export class SimpleListInputComponent implements OnInit {
     });
   }
 
+  selectValue(item: string): void {
+    this.selectedValue = item;
+  }
 }
